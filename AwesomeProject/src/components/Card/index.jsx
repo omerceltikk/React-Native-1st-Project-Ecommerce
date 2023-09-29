d8ea508e88b0a0df03c747049ff25c4fc0f97fb4
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-09-29 14:18:02 +0300

    application search events done

--- AwesomeProject/src/components/Card/index.jsx
+++ AwesomeProject/src/components/Card/index.jsx
@@ -1,20 +1,69 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,Image, Dimensions} from 'react-native'
 
+// const win = Dimensions.get('window');
 const Card = ({product}) => {
+
   return (
-    <View style={styles.Cardstyle}>
-      <Text>
-        {product.id}
+    <View style={styles.cardStyle}>
+     <Image
+     src={product.image}
+     style={styles.imageStyle}
+     />
+     <View style={styles.cardText}>
+      <Text style={styles.title}>
+        {product.title}
       </Text>
+      <Text style={styles.description}>
+        {product.description.length > 100 ? product.description.slice(0,100) : product.description}...
+      </Text>
+      <Text style={styles.price}>
+        {product.price} $
+      </Text>
+      </View> 
     </View>
   )
 }
 const styles = StyleSheet.create({
-  Cardstyle:{
-    margin: 20,
-    padding: 20,
-    
+  cardStyle:{
+    marginHorizontal:20,
+    marginVertical:10,
+    padding: 10,
+    alignItems:"center",
+    flexDirection:"row",
+    backgroundColor:"#ffffff",
+    borderRadius:20,
+    shadowColor: "#000",
+    shadowOffset:{width:30,height:30},
+    overflow:"scroll"
+  
+  },
+  imageStyle:{
+    height:140,
+    width:120,
+    padding:10,
+    margin:10
+  },
+  cardText:{
+    flexDirection:"column",
+    flex:1,
+    padding:10,
+    gap:10,
+  
+  },
+  description:{
+    fontStyle:"italic",
+    fontWeight:"400",
+    color:"#adadad"
+  },
+  title:{
+    fontWeight:"700"
+  },
+  price:{
+    color:"#8EACCD",
+    marginTop:10,
+    textAlign:"right",
+    fontWeight:"700"
   }
 })
 export default Card
--- AwesomeProject/src/pages/ListPage/index.jsx
+++ AwesomeProject/src/pages/ListPage/index.jsx
@@ -1,24 +1,89 @@
-import React, { useState } from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, TextInput} from 'react-native'
 import Card from '../../components/Card'
 
 const ListPage = () => {
   const [store, setStore] = useState(null);
-  async function FetchData(){
-    await fetch("https://fakestoreapi.com/products").then((res) => res.json()).then((res) => setStore(res));
-    
+  const [search, setSearch] = useState(null);
+  useEffect(() => {
+    async function FetchData() {
+      await fetch("https://fakestoreapi.com/products").then((res) => res.json()).then((res) => setStore(res));
+    }
+    FetchData();
+  }, [])
+
+  function HandleChange(e){
+   if(e.length >= 1 && store){
+   let filtered = store.filter((item) => item.title.toLowerCase().includes(e.toLowerCase()))
+   setSearch(filtered);
+   }
+   else if(e.length < 1){
+    setSearch(null);
+   }
   }
-  FetchData();
   return (
-    <View>
-      {store == null ? <Text>Loading...</Text> : 
-      store?.map((item) => (
-        <Card key={item.id} product={item}/>
-        ))
+    <View style={styles.ListPage}>
+      <View style={styles.searchArea}>
+        <Text style={styles.header}>
+         First App Market
+        </Text>
+        
+        <Text style={styles.title}>
+          Find Your Maches!!
+        </Text>
+      <TextInput
+        style={styles.input}
+        blurOnSubmit={true}
+        inputMode="search"
+        onChangeText={(e) => HandleChange(e)}
+        // value={text}
+      />
+      </View>
+      {store == null ? <Text>Loading...</Text> :
+        search?.length > 0 && search != null &&
+        search?.map((item) => (
+          <Card key={item.id} product={item} />
+        )) ||
+         search == null &&
+        store?.map((item) => (
+          <Card key={item.id} product={item} />
+        )) ||
+        search.length == 0 && <Text>No Matches Found!</Text>
       }
-      
+
     </View>
   )
 }
+const styles = StyleSheet.create({
+  ListPage: {
+    
+    backgroundColor: "#fdf5eb"
+  },
+  searchArea:{
+    margin:20,
+    
+  },
+  title:{
+    fontWeight:"600",
+    paddingLeft:10,
+    fontSize:15,
+    color:"#B9B4C7",
+    margin:10,
+  },
+  header:{
+    fontWeight:"800",
+    fontSize:30,
+    paddingLeft:10,
+    color:"#5C5470",
+    margin:10
 
+  },
+  input:{
+    borderRadius:100,
+    paddingHorizontal:20,
+    backgroundColor:"#fff",
+    borderColor:"#e7e7e7",
+    borderWidth:1
+  },
+})
 export default ListPage
